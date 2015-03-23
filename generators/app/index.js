0cd4bd58b2e56b91be34f39ef656862dfc640635
John Henry <john@iamjohnhenry.com>
2015-03-23 13:01:45 -0700

    Add default to files

--- generators/app/index.js
+++ generators/app/index.js
@@ -13,37 +13,33 @@ module.exports = yeoman.generators.Base.extend({
 
     // Have Yeoman greet the user.
     this.log(yosay(
-      'Let\'s create an ' + chalk.blue('Optimizely Experiment') + '... Funnelenvy Style!'
+      'Let\'s create an ' + chalk.blue('Optimizely Project') + '... Funnelenvy Style!'
     ));
 
     var prompts = [
       {
-      type: 'input',
-      name: 'name',
-      message: 'What\'s the name of your experiment',
-      default: 'Default Experiment'
+        type: 'input',
+        name: 'name',
+        message: 'Name your project folder',
       },
       {
-      type: 'input',
-      name: 'description',
-      message: 'What\'s the your experiment\'s description',
-      default: 'Default Experiment'
-      }
-      ,
+        type: 'input',
+        name: 'id',
+        message: 'What\'s your projects\'s ID?',
+      },
       {
-      type: 'input',
-      name: 'url',
-      message: 'What\'s your experiments edit URL?',
-      default: 'http://www.funnelenvy.com'
-      }
-      ,
+        type: 'password',
+        name: 'token',
+        message: 'What\'s your API \'s token? (Copy + Paste -- Will not be shown.)',
+      },
       {
-      type: 'input',
-      name: 'id',
-      message: 'What\'s your experiment\'s ID? (Leave blank if not yet created.)',
-      default: ''
+        type: 'confirm',
+        name: 'include_jquery',
+        message: 'Include jQuery?',
+        default:true,
       }
     ];
+
     this.prompt(prompts, function (props) {
       this.props = props;
       done();
@@ -53,26 +49,27 @@ module.exports = yeoman.generators.Base.extend({
   writing: {
     app: function () {
       var props = this.props;
-
-      this.fs.copyTpl(//Create experiment.json
-        this.templatePath('_experiment.json'),
-        this.destinationPath(props.name + '/experiment.json'),
+      this.fs.copyTpl(//Create gulpfile.js
+        this.templatePath('_token'),
+        this.destinationPath(props.name + '/.optcli/token'),
         props
-      )
-      this.fs.copyTpl(//Create global.js
-        this.templatePath('_empty'),
-        this.destinationPath(props.name + '/global.js'),
+      );
+      this.fs.copyTpl(//Create gulpfile.js
+        this.templatePath('_project.json'),
+        this.destinationPath(props.name + '/project.json'),
         props
-      )
-      this.fs.copyTpl(//Create global.css
-        this.templatePath('_empty'),
-        this.destinationPath(props.name + '/global.css'),
-        props
-      )
+      );
+    },
+    projectfiles: function () {
+      var props = this.props;
+      this.fs.copy(
+        this.templatePath('editorconfig'),
+        this.destinationPath(props.name + '/.editorconfig')
+      );
+      this.fs.copy(
+        this.templatePath('jshintrc'),
+        this.destinationPath(props.name + '/.jshintrc')
+      );
     }
-  },
-
-  install: function () {
-    console.log('Don\'t forget to run "npm install" in your directory')
   }
 });
